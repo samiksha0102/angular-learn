913f916722492a78643c6493b03ca97f3170338d
capgjava01@iiht.tech <capgjava01@iiht.tech>
2020-02-28 21:29:20 +0530

    Configuring angular 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +23,10 @@ import {MatListModule} from '@angular/material/list';
     BrowserAnimationsModule,
     FlexLayoutModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from '../_shared/dish';
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-dishes:Dish[] = [ {
+const DISHES: Dish[] = [ {
   id: '0',
   name: 'Uthappizza',
   image: '/assets/images/uthappizza.png',
@@ -47,6 +41,14 @@ dishes:Dish[] = [ {
   price: '2.99',
   description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
 }];
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+dishes:Dish[] = DISHES;
+selectedDish:Dish = DISHES[0];
   constructor() { }
 
   ngOnInit(): void {
